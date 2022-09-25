d487130e9be8518a62d34e6fa14e234cf1271c51
BoGGoG <marco.knipfer@me.com>
2022-09-25 20:03:20 +0200

    QCD data preparation added

--- data-generation-marty/QED/QED_loop_insertions_parallel.py
+++ data-generation-marty/QED/QED_loop_insertions_parallel.py
@@ -279,11 +279,11 @@ if __name__== "__main__":
                     format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
 
-    run_all_n_to_m_parallel(particles_list, 1, 2, folders, cpu_cores=cpu_cores)
+    # run_all_n_to_m_parallel(particles_list, 1, 2, folders, cpu_cores=cpu_cores)
     # run_all_n_to_m_parallel(particles_list, 2, 1, folders, cpu_cores=cpu_cores)
     # run_all_n_to_m_parallel(particles_list, 2, 2, folders, cpu_cores=cpu_cores)
     # run_all_n_to_m_parallel(particles_list, 3, 1, folders, cpu_cores=cpu_cores)
     # run_all_n_to_m_parallel(particles_list, 1, 3, folders, cpu_cores=cpu_cores)
     # run_all_n_to_m_parallel(particles_list, 3, 2, folders, cpu_cores=cpu_cores)
-    # run_all_n_to_m_parallel(particles_list, 2, 3, folders, cpu_cores=cpu_cores)
+    run_all_n_to_m_parallel(particles_list, 2, 3, folders, cpu_cores=cpu_cores)
     # run_all_n_to_m_parallel(particles_list, 3, 3, folders, cpu_cores=cpu_cores)
