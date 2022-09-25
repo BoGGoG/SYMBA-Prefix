8d5f4c2ca6a317180630e28fba82955e631cc58f
BoGGoG <marco.knipfer@me.com>
2022-09-25 11:07:13 +0200

    Readme modified

--- sympy-prefix/README.md
+++ sympy-prefix/README.md
@@ -3,6 +3,21 @@ This is a package for converting [sympy](https://www.sympy.org/en/index.html)
 expressions to prefix notation.
 There is standard prefix notation and a custom one with shorter sequences.
 
+# What?
+It's best explained with an example:
+The sympy expression
+```a + b*c```
+is transformed to the list of strings
+```['add', 'a', 'mul', 'b', 'c']```
+This is equivalent to a depth first traversal of the tree
+```mermaid
+graph TD
+add[add] --> a[a]
+add --> mul[mul]
+mul --> b
+mul --> c
+```
+
 # Why?
 Recently in Machine Learning there has been a seminal paper on [Deep Learning for Symbolic Mathematics](https://arxiv.org/abs/1912.01412)
 where the authors show that with neural networks they can symbolically solve tasks like integration
--- sympy-prefix/dev/hybrid_prefix_to_sympy.py
+++ sympy-prefix/dev/hybrid_prefix_to_sympy.py
@@ -18,13 +18,32 @@ with open(sqampls_file) as f:
 
 ic(len(sqampls))
 
+test_sqampls = [
+        "a+b*c",
+        # "8*g**4*(2*m**4 - m**2*s)",
+        # "8*g**4*(2*m**4 - m**2*(s+d))",
+        # "8*g**4*(2*m**4 - m**2*(s+d)**2)",
+        # "25*21*a**(b*c)",
+        # "exp(3)" # not working yet, but not crucial for SYMBA. TODO
+        # "sin(x)" # not working yet, but not crucial for SYMBA. TODO
+        ]
+for test_sqampl in test_sqampls:
+    test_sqampl_sp = sp.factor(sp.sympify(test_sqampl))
+    test_sqampl_prefix = sympy_to_hybrid_prefix(test_sqampl_sp)
+    rec = hybrid_prefix_to_sympy(test_sqampl_prefix)
+    ic(test_sqampl_sp)
+    ic(np.array(test_sqampl_prefix))
+    ic(rec == test_sqampl_sp)
+
 num_tests = 10
 ctr = 0
 for test_sqampl in np.random.choice(sqampls, num_tests):
     test_sqampl_sp = sp.factor(sp.sympify(test_sqampl))
     test_sqampl_prefix = sympy_to_hybrid_prefix(test_sqampl_sp)
     rec = hybrid_prefix_to_sympy(test_sqampl_prefix)
-    ic(test_sqampl)
+    # ic(test_sqampl)
+    ic(test_sqampl_sp)
+    ic(np.array(test_sqampl_prefix))
     ic(rec == test_sqampl_sp)
     ctr = ctr + 1
 ic(ctr)
